a0b63b492266ed086c9a124fdb3da548b4327c71
Howard Timlin <howardtimlin@gmail.com>
2020-08-13 17:09:30 -0500

    much cleaner, better output

--- stochastic_process.py
+++ stochastic_process.py
@@ -4,32 +4,55 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 
+numTransitions = 2 # maxium number of transition rates leaving any point in the unit cell
+
+
+numCells = 32 # number of unit cells to be simulated
+displayWidth = 32 # number of cells wide the display will be
+
+
+iterations = 500 # number of iterations for the simulation
+timeStep = 1/5 # time step for each iteration of the simulation
+delay = 1000/iterations # delay between frames in animation
+
+
+displayMode = 2 # display mode (1-3)
+# 1 - displays the value of the first point in each unit cells
+# 2 - displays the value of all points in each unit cell as verticle blocks (best for 1D simulations)
+# 3 - displays the value of the first three points in each unit cell as the three color channels
+
+
+reactions = np.array( [ [1,0], [0,2], [1,0] ] ) # array representing the set of reactions
+# collumn vectors are the multiplicities of each substance in each complex
+# e.g. 2X+Y <-> 3Y would have reaction matrix [[2,0],[1,3]]
+
+fSize = reactions.shape[1] # size of the fundamnetal domain of each unit cell, i.e. number of complexes in the set of reactions
+numMolecules = reactions.shape[0] # number of differnt molecules in the set of reactions
+psiWidth = displayWidth * fSize # width of each row of the display in terms the psi vector/W matrix
+
+
+# Under the duality presented in my email, a1 <-> b1 and a2 <-> b2 under the duality transformation
+a1 = [.9, .75, .5, .25, .1]
+a2 = [.8, .6, .5, .4, .2]
+b1 = np.ones(len(a1))-a1
+b2 = np.ones(len(a2))-a2
+
+
+# transition rate matrix duality operator
+U = np.zeros((numCells * fSize, numCells * fSize))
+for n in range(numCells * fSize):
+    for m in range(numCells * fSize):
+        if (n + m) == (numCells * fSize - 2):
+            U[n,m] = 1
+
+
+Y = np.kron(np.eye(numCells,dtype=int),reactions) # tile reaction matrix along the diagonal to create matrix "projecting" back down to observable concentrations
+Yinverse = np.kron(np.eye(numCells,dtype=int), np.linalg.pinv(reactions))
+
+V = np.matmul(Y,np.matmul(U,Yinverse))
+
+
 def main():
-    numTransitions = 2 # maxium number of transition rates leaving any point in the unit cell
-
-
-    numCells = 8 # number of unit cells to be simulated
-    displayWidth = 8 # number of cells wide the display will be
-
-
-    iterations = 100 # number of iterations for the simulation
-    timeStep = 1/5 # time step for each iteration of the simulation
-    delay = 1000/iterations # delay between frames in animation
-
-
-    displayMode = 2 # display mode (1-3)
-    # 1 - displays the value of the first point in each unit cells
-    # 2 - displays the value of all points in each unit cell as verticle blocks (best for 1D simulations)
-    # 3 - displays the value of the first three points in each unit cell as the three color channels
-
-
-    reactions = np.array( [ [1,0], [0,2], [1,0] ] ) # array representing the set of reactions
-    # collumn vectors are the multiplicities of each substance in each complex
-    # e.g. 2X+Y <-> 3Y would have reaction matrix [[2,0],[1,3]]
-
-    fSize = reactions.shape[1] # size of the fundamnetal domain of each unit cell, i.e. number of complexes in the set of reactions
-    numMolecules = reactions.shape[0] # number of differnt molecules in the set of reactions
-    psiWidth = displayWidth * fSize # width of each row of the display in terms the psi vector/W matrix
 
     cell = np.zeros((fSize, numTransitions, 2)) # matrix storing transition rates for each unit cell
     # 1st dimension - specifies the point in the unit cells
@@ -37,35 +60,19 @@ def main():
     # 3rd dimension - specifies the rate and the relative position of the point being transitioned to
     # (adding "fSize" translates one unit cell to the right and adding "psiWidth" translates one unit cell down)
 
-
-    # Under the duality presented in my email, a1 <-> b1 and a2 <-> b2 under the duality transformation
-    a1 = [.9, .75, .5, .25, .1]
-    a2 = [.8, .6, .5, .4, .2]
-    b1 = np.ones(len(a1))-a1
-    b2 = np.ones(len(a2))-a2
-
-
-    U = np.zeros((numCells * fSize, numCells * fSize))
-    for n in range(numCells * fSize):
-        for m in range(numCells * fSize):
-            if (n + m) == (numCells * fSize - 2):
-                U[n,m] = 1
-
-
-    Y = np.kron(np.eye(numCells,dtype=int),reactions) # tile reaction matrix along the diagonal to create matrix "projecting" back down to observable concentrations
-    Yinverse = np.kron(np.eye(numCells,dtype=int), np.linalg.pinv(reactions))
-
-    V = np.matmul(Y,np.matmul(U,Yinverse))
-    Vinverse = np.matmul(Y,np.matmul(np.linalg.pinv(U),Yinverse))
-
-    arr = [ [ [] for _ in range(len(a1)) ] for _ in range(3) ] # lists storing each frame of the resulting animations
+    arr = [ [ [] for _ in range(len(a1)) ] for _ in range(3) ] # list storing each frame of the resulting animations
 
 
     a = np.zeros((numCells * numMolecules, 1))
     b = np.zeros((numCells * numMolecules, 1))
     p0 = np.zeros((numCells * numMolecules, 1))
-    Vp0 = np.zeros((numCells * numMolecules, 1))
 
+    # initialize the probability/concentration vector
+    for i in range(numMolecules):
+        p0[int(numCells * numMolecules / 2 - i - 1)] = 1/(numMolecules)
+
+    print("|V p_0 - p_0| :")
+    print(np.linalg.norm(np.add(p0,np.matmul(V,p0) * (-1))))
 
     for r in range(len(a1)):
 
@@ -75,14 +82,8 @@ def main():
 
 
         p = np.zeros((numCells * numMolecules, 1)) # probability/concentration vector
+        Vp = np.zeros((numCells * numMolecules, 1))
         W = np.zeros((numCells * fSize, numCells * fSize)) # transition/reaction rate matrix
-
-
-        # initialize the probability/concentration vector
-        for i in range(numMolecules):
-            p0[int(numCells * numMolecules / 2 - i - 1)] = 1/(numMolecules)
-
-        Vp0 = np.matmul(V,p0)
 
 
         # initialize W matrix from transition rates in "cell" matrix
@@ -100,60 +101,31 @@ def main():
 
 
         # convert the probability/concentration vector into the first frame of the animation
-        arr[1][r].append(vectorToFrame(p0, numCells, numMolecules, displayWidth, displayMode))
-        arr[2][r].append(vectorToFrame(p0, numCells, numMolecules, displayWidth, displayMode))
+        arr[0][r].append(vectorToFrame(p0))
+        arr[1][r].append(vectorToFrame(p0))
+        arr[2][r].append(vectorToFrame(p0))
 
 
         p = p0
+        Vp = np.matmul(V,p0)
 
         # iterate simulation and render to animation
         for i in range(iterations):
 
-            psi = np.ones((numCells * fSize, 1)) # vector of complex concentrations
+            p = iterateVector(p, W)
+            Vp = iterateVector(Vp, W)
 
-            # initialize complex concentration vector
-            for j in range(fSize):
-                for k in range(numMolecules):
-                    psi[j::fSize] = np.multiply(psi[j::fSize], np.power(p[k::numMolecules], reactions[k, j]))
-
-            if r == 0 and i == 0:
-                print("|U.psi(0) - psi(0)| :")
-                print(np.linalg.norm(np.add(psi,np.matmul(U,psi) * (-1))))
-
-
-            # iterate simulation via master equation
-            p = np.add(p, np.matmul(Y, np.matmul(W, psi)) * timeStep)
-
-
-            if r == 0 and i == iterations-1:
-                a = p
+            if i == iterations-1:
+                if r == 0:
+                    a = np.matmul(V,p)
+                elif r == len(a1)-1:
+                    b = Vp
 
             # convert probability/concentration vector into frame of animation dependent on display mode
-            arr[1][r].append(vectorToFrame(p, numCells, numMolecules, displayWidth, displayMode))
+            arr[0][r].append(vectorToFrame(p))
+            arr[1][r].append(vectorToFrame(np.matmul(V,p)))
+            arr[2][r].append(vectorToFrame(Vp))
 
-
-
-        p = Vp0
-
-        for i in range(iterations):
-
-            psi = np.ones((numCells * fSize, 1)) # vector of complex concentrations
-
-            # initialize complex concentration vector
-            for j in range(fSize):
-                for k in range(numMolecules):
-                    psi[j::fSize] = np.multiply(psi[j::fSize], np.power(p[k::numMolecules], reactions[k, j]))
-
-
-            # iterate simulation via master equation
-            p = np.add(p, np.matmul(Y, np.matmul(W, psi)) * timeStep)
-
-
-            if r == len(a1)-1 and i == iterations-1:
-                b = np.matmul(Vinverse,p)
-
-            # convert probability/concentration vector into frame of animation dependent on display mode
-            arr[2][r].append(vectorToFrame(np.matmul(Vinverse,p), numCells, numMolecules, displayWidth, displayMode))
 
 
     print("norm of difference between estimated and actual p(t):")
@@ -184,18 +156,27 @@ def main():
 
     # gather frames of animation into animated plot
     fig = plt.figure()
-    axes = [ fig.add_subplot(len(a1),2,r+1) for r in range(2 * len(a1)) ]
+    axes = [ fig.add_subplot(len(a1),4,r+1) for r in range(4 * len(a1)) ]
     axes[0].set_title('E(t)[x(0)]')
-    axes[1].set_title('V^{-1}E*(t)[Vx(0)]')
+    axes[1].set_title('VE*(t)[x(0)]')
+    axes[2].set_title('E(t)[Vx(0)]')
+    axes[3].set_title('| VE*(t)[x(0)] - E(t)[Vx(0)] |')
 
     ims = []
     for i in range(iterations+1):
         im = []
-        for r in range(2 * len(a1)):
-            if r % 2 == 0:
-                im.append(axes[r].imshow(arr[1][int(r/2)][i]))
+        for r in range(4 * len(a1)):
+            if r % 4 == 0:
+                im.append(axes[r].imshow(arr[0][int(r/4)][i]))
+            elif r % 4 == 1:
+                im.append(axes[r].imshow(arr[1][len(a1)-1-int((r-1)/4)][i]))
+            elif r % 4 == 2:
+                im.append(axes[r].imshow(arr[2][int((r-2)/4)][i]))
             else:
-                im.append(axes[r].imshow(arr[2][len(a1)-1-int((r-1)/2)][i]))
+                difference = np.add(np.ones((int(numCells / displayWidth), displayWidth, 3)), np.abs(np.add(arr[1][int((r-3)/4)][i], arr[2][len(a1)-1-int((r-3)/4)][i] * (-1))) * (-1))
+                difference[:,:,0] = difference[:,:,1]
+                difference[:,:,2] = np.ones((int(numCells / displayWidth), displayWidth))
+                im.append(axes[r].imshow(difference))
         ims.append(im)
 
     ani = animation.ArtistAnimation(fig, ims, interval=delay, blit=True)
@@ -207,7 +188,21 @@ def main():
     plt.show()
 
 
-def vectorToFrame(vec, numCells, numMolecules, displayWidth, displayMode):
+def iterateVector(vec, W):
+    psi = np.ones((numCells * fSize, 1)) # vector of complex concentrations
+
+    # initialize complex concentration vector
+    for j in range(fSize):
+        for k in range(numMolecules):
+            psi[j::fSize] = np.multiply(psi[j::fSize], np.power(vec[k::numMolecules], reactions[k, j]))
+
+
+    # iterate simulation via master equation
+    return np.add(vec, np.matmul(Y, np.matmul(W, psi)) * timeStep)
+
+
+def vectorToFrame(vec):
+    vec = np.abs(vec)
     if displayMode == 1:
         frame = np.ones((int(numCells / displayWidth), displayWidth, 3))
 
@@ -226,6 +221,7 @@ def vectorToFrame(vec, numCells, numMolecules, displayWidth, displayMode):
                 #frame[numMolecules*j + k, :, 0] = 1 - vec[j * displayWidth * numMolecules + k : (j+1) * displayWidth * numMolecules : numMolecules, 0]
                 frame[numMolecules*j + k, :, 1] = 1 - vec[j * displayWidth * numMolecules + k : (j+1) * displayWidth * numMolecules : numMolecules, 0]
                 frame[numMolecules*j + k, :, 2] = 1 - vec[j * displayWidth * numMolecules + k : (j+1) * displayWidth * numMolecules : numMolecules, 0]
+
 
         return frame
 
